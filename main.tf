21b5bf3c63ad44555a9a5251b7f47a1d677395a8
Lucy Davinhart <lucy@lucymhdavies.com>
2024-04-16 14:54:09 +0100

    Use the proxy for Plugin WIF

--- main.tf
+++ main.tf
@@ -9,11 +9,14 @@ terraform {
 }
 
 variable "oidc_audience" {
-  default = "vault.lmhd.me:443/v1/identity/oidc/plugins"
+  #  default = "vault.lmhd.me:443/v1/identity/oidc/plugins"
+  default = "vault-plugin-wif.lucy-davinhart.sbx.hashidemos.io:443/v1/demos/plugin-wif/identity/oidc/plugins"
 }
 variable "vault_plugins_addr" {
-  default = "https://vault.lmhd.me/v1/identity/oidc/plugins"
+  # default = "https://vault.lmhd.me/v1/identity/oidc/plugins"
+  default = "https://vault-plugin-wif.lucy-davinhart.sbx.hashidemos.io/v1/demos/plugin-wif/identity/oidc/plugins"
 }
+
 
 
 # https://developer.hashicorp.com/vault/docs/secrets/aws#plugin-workload-identity-federation-wif
@@ -54,16 +57,66 @@ EOF
   }
 }
 
+
+provider "vault" {
+  namespace = "demos/plugin-wif/"
+}
+
+
+
+# TODO: identity/oidc/config
+
+
+
+
+# BEGIN HASHI-SPECIFIC
+locals {
+  username_template = <<EOT
+{{ if (eq .Type "STS") }}
+	{{ printf "demo-lucy.davinhart@hashicorp.com-vault-%s-%s" (random 20) (unix_time) | truncate 32 }}
+{{ else }}
+	{{ printf "demo-lucy.davinhart@hashicorp.com-vault-%s-%s" (unix_time) (random 20) | truncate 60 }}
+{{ end }}
+EOT
+
+  # Known good config
+  #    {{ printf "${aws_iam_user.vault_mount_user.name}-vault-%s-%s" (unix_time) (random 20) | truncate 60 }}
+  #
+  # Template from https://developer.hashicorp.com/vault/api-docs/secret/aws#username_template
+  #    {{ printf "vault-%s-%s-%s" (printf "%s-%s" (.DisplayName) (.PolicyName) | truncate 42) (unix_time) (random 20) | truncate 64 }}
+  # I can't get that to work, so... Known Good is fine for now
+
+  username_template_without_whitespace = replace(
+    replace(
+      local.username_template,
+      "\n", ""
+    ),
+    "\t", ""
+  )
+}
+data "aws_iam_policy" "demo_user_permissions_boundary" {
+  name = "DemoUser"
+}
+# END HASHI-SPECIFIC
+
+
 resource "vault_aws_secret_backend" "aws" {
-  path                    = "aws/lmhd/test-oidc"
+  path                    = "aws/hashi"
   identity_token_audience = var.oidc_audience
   role_arn                = aws_iam_role.plugins_role.arn
+
+
+  # Hashi-specific requirement
+  username_template = local.username_template_without_whitespace
 }
 
 resource "vault_aws_secret_backend_role" "test" {
   backend         = vault_aws_secret_backend.aws.path
   name            = "test"
   credential_type = "iam_user"
+
+  permissions_boundary_arn = data.aws_iam_policy.demo_user_permissions_boundary.arn
+
 
   policy_document = <<EOT
 {
@@ -80,4 +133,9 @@ resource "vault_aws_secret_backend_role" "test" {
     ]
 }
 EOT
+}
+
+
+output "test_command" {
+  value = "vault read ${vault_aws_secret_backend.aws.path}/creds/${vault_aws_secret_backend_role.test.name}"
 }
